51222029560566d44f3ff00a07694f43ab8a4c41
Brandon Zhu <bzhu@utraque.com>
2021-01-24 15:17:20 -0500

    131.  Converting Errors to Responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -8,15 +8,22 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if(err instanceof RequestValidationError) {
-    console.log('handling this error as a request validation error');
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
-  
-  else if(err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a database connection error');
+
+  else if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({
+      errors: [
+        { message: err.reason }
+      ]
+    });
   }
 
   res.status(400).send({
-    message: err.message
+    errors: [{ message: 'Something went wrong' }];
   });
 };
