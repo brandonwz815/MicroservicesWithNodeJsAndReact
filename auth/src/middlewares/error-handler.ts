4944b7a3de457eaf4a7a2dfc8855df95f7c8ed7b
Brandon Zhu <bzhu@utraque.com>
2021-01-24 14:54:21 -0500

    130.  Determining Error Type

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
-  err: Error, 
-  req: Request, 
-  res: Response, 
+  err: Error,
+  req: Request,
+  res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if(err instanceof RequestValidationError) {
+    console.log('handling this error as a request validation error');
+  }
+  
+  else if(err instanceof DatabaseConnectionError) {
+    console.log('handling this error as a database connection error');
+  }
 
   res.status(400).send({
-    message: 'Something went wrong'
+    message: err.message
   });
 };
