8e41875da824b5ff3ef4ff34e71abe1370448640
Brandon Zhu <bzhu@utraque.com>
2021-02-01 23:55:02 -0500

    179.  Augmenting Type Definitions

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,6 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 export const currentUser = (
   req: Request,
   res: Response,
@@ -11,10 +24,8 @@ export const currentUser = (
   }
 
   try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    req.currentser = payload;
-  } catch (err) {
-
-  }
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+    req.currentUser = payload;
+  } catch (err) { }
   next();
 };
