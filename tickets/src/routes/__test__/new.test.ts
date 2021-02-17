4a3bc89e2216c76f576226ef2323abea54e32055
Brandon Zhu <bzhu@utraque.com>
2021-02-17 00:40:24 -0500

    258.  Creation via Route Handler

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
   const response = await request(app)
@@ -64,14 +65,21 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
-  // add in a check to ensure that a ticket is created
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
 
+  const title =  'asfd';
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'asfd',
+      title,
       price: 25
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(25);
+  expect(tickets[0].title).toEqual(title);
 });
