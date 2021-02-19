fcb761cd999fcb049937b1b0a9cce2f455c83de2
Brandon Zhu <bzhu@utraque.com>
2021-02-19 02:28:54 -0500

    267.  Final Update Changes

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { setOriginalNode } from 'typescript';
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = mongoose.Types.ObjectId().toHexString();
@@ -46,9 +47,59 @@ it('returns a 401 if the user does not own the ticket', async () => {
 });
 
 it('returns a 400 if the user provides an invalid title and price', async () => {
+  const cookie = global.signin();
 
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'asfdsdf',
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: -20
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'asfdasf',
+      price: -10
+    })
+    .expect(400);
 });
 
-it('returns a 400 if the user provides an invalid title or price', async () => {
+it('updates the ticket provided valid inputs', async () => {
+  const cookie = global.signin();
 
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({
+      title: 'asdfs',
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('new title');
+  expect(ticketResponse.body.price).toEqual(100);
 });
