7332942806719190051e8f1eedddf27d35102126
Brandon Zhu <bzhu@utraque.com>
2021-02-19 00:09:34 -0500

    262.  Better Error Logging

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,10 +1,12 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 it('returns a 404 if the ticket is not found', async () => {
+  const id = mongoose.Types.ObjectId().toHexString();
+  
   await request(app)
-    .get('/api/tickets/asfdasf')
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
 });
@@ -28,4 +30,4 @@ it('returns the ticket if the ticket is found', async () => {
 
   expect(ticketResponse.body.title).toEqual(title);
   expect(ticketResponse.body.price).toEqual(price);
-})
+});
