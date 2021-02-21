8dfd38ef197596abf6adbb5e7fd11017080c315c
Brandon Zhu <bzhu@utraque.com>
2021-02-21 13:55:42 -0500

    315.  Successful Listen!

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -24,12 +24,13 @@ router.post(
       userId: req.currentUser!.id
     });
     await ticket.save();
+
     await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
-      price: title.price,
+      price: ticket.price,
       userId: ticket.userId
-    })
+    });
 
     res.status(201).send(ticket);
   }
