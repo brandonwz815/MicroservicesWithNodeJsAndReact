fd93f6ce1908a61c9c8d94922b6e316c28785c42
Brandon Zhu <bzhu@utraque.com>
2021-02-22 00:04:59 -0500

    324.  NATS Env Variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,11 +7,24 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined');
   }
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be define');
+    throw new Error('MONGO_URI must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
   }
 
   try {
-    await natsWrapper.connect('ticketing', 'slkdfj', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID, 
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+      );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
